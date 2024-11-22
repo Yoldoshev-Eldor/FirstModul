41e3f2de9d00ab7f0f137638c1e0a00db1ba1df0
ibragimovdev7 <89067487+ibragimovdev7@users.noreply.github.com>
2024-11-22 17:05:09 +0500

    Lesson 8 Students CRUD uploaded

---
+++ Homework/Lesson8_Students_Crud/Model/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8_Person_Crud.Model
+{
+    internal class Person
+    {
+    }
+}
---
+++ Homework/Lesson8_Students_Crud/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson8_Person_Crud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
